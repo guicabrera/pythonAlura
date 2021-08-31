4f062f63e52214ef2e3d46d0b870a8a12501b365
G Cabrera <guilherme.cabrera@edu.ung.br>
2021-08-30 22:33:35 -0300

    string python

---
+++ Python - Strings/sec1String.py
@@ -0,0 +1,7 @@
+#para fatiar uma string basta:
+
+strin = "minha string"
+
+print(strin[0:6])
+
+#o primeiro valor é a origem e o segundo valor significa o final (que possui final exclusivo e inicio inclusivo)
